79ca6c0940e9d2b60ea90e9bdc117ee88333704e
Abdur Rahim <rahim11500000@gmail.com>
2024-04-26 22:07:35 +0600

    make tourists spot api for single data load

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9d7z5qs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,6 +37,12 @@ async function run() {
             const result = touristsSpotCollection.find();
             const allTouristsSpotData = await result.toArray();
             res.send(allTouristsSpotData)
+        })
+        app.get("/tourists-spot/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const touristsSpot = await touristsSpotCollection.findOne(query);
+            res.send(touristsSpot)
         })
 
         // Send a ping to confirm a successful connection
